2fc24995d16d5654aa5820b6dde7c1034727e3c2
HarshSulakhe <sulakheharsh@gmail.com>
2020-05-06 02:18:38 +0530

    Define encoder and decoder blocks for UNet

--- Pix2Pix/pix2pix-keras.py
+++ Pix2Pix/pix2pix-keras.py
@@ -6,6 +6,9 @@ from keras.initializers import RandomNormal
 #discriminator takes in real input,sampled from dataset and generated input
 #produces a 30x30x1 tensor
 #PatchGAN architecture
+image_shape = (256,256,3)
+
+
 def define_discriminator(image_shape):
     #initialize weights with std = 0.2
     init = RandomNormal(stddev = 0.2)
@@ -40,8 +43,24 @@ def define_discriminator(image_shape):
     model.compile(optimizer = opt,loss = 'binary_crossentropy',loss_weights = [0.5])
     return model
 
-image_shape = (256,256,3)
-
 discriminator = define_discriminator(image_shape)
-
 discriminator.summary()
+
+def define_encoder(input_tensor,n_filters,batchnorm = True):
+    init = RandomNormal(stddev = 0.2)
+
+    x = Conv2D(n_filters,(4,4),strides = (2,2),padding = 'same',kernel_initializer = init)(input_tensor)
+    if batchnorm:
+        x = BatchNormalization()(x,training = True)
+    x = LeakyReLU(0.2)(x)
+    return x
+
+def define_decoder(input_tensor,n_filters,skip_tensor,dropout = True):
+    init = RandomNormal(stddev = 0.2)
+
+    x = Conv2DTranspose(n_filters,(4,4),strides = (2,2),padding = 'same',kernel_initializer = init)(input_tensor)
+    x = BatchNormalization()(x,training = True)
+    if dropout:
+        x = Dropout(0.4)(x,training = True)
+    x = Activation('relu')(x)
+    return x
